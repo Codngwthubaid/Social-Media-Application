17d72badaa210852718beed26ce99871f19037f7
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-15 09:08:34 +0530

    Authentication completes for pot Uploading

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -6,5 +6,11 @@ exports.isAuthenticate = async (req, res, next) => {
 
     // Taking Token
     const { token } = req.cookie;
-    if (!token) return res.status(401).json({ message: "Please Login First ..." })
+    // Token Presence Checking
+    if (!token) return res.status(401).json({ message: "Please login First ..." })
+    //decode token process 
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+    // find user by ID
+    req.user = await User.findById(decoded._id)
+    next()
 }
--- Backend/routes/Post.js
+++ Backend/routes/Post.js
@@ -1,8 +1,9 @@
 // API routes creation and using controller func's 
 const express = require("express");
 const { createPost } = require("../controller/Post");
+const { isAuthenticate } = require("../middlewares/auth");
 const router = express.Router()
 
-router.route("/post/upload").post(createPost);
+router.route("/post/upload").post(isAuthenticate, createPost);
 
 module.exports = router
