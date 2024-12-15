48cb0f365ff11d692defc0a3ae8ef79af1692d86
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-15 10:02:16 +0530

    remove unnecessary log's from auth

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -6,7 +6,6 @@ exports.isAuthenticate = async (req, res, next) => {
     try {
         // Taking Token
         const { token } = req.cookies;
-
         // Token Presence Checking
         if (!token) return res.status(401).json({ message: "Please login First ..." })
         //decode token process 
