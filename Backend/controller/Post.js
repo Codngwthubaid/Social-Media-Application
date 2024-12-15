ace70bf6a45556c56771d2c600bc14dc1ea11204
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-15 18:59:46 +0530

    Create PostDeletion and LikeUnlikePost func's

--- Backend/controller/Post.js
+++ Backend/controller/Post.js
@@ -1,7 +1,7 @@
 const Post = require("../models/Post")
 const User = require("../models/User")
 
-// Post Creation
+// Post Creation [post request]
 exports.createPost = async (req, res) => {
     try {
         // Post details
@@ -37,18 +37,28 @@ exports.createPost = async (req, res) => {
 }
 
 
-// Post Deletion
+// Post Deletion [delete request]
 exports.deletePost = async (req, res) => {
     try {
         // finding post 
         const post = await Post.findById(req.params.id)
+
         // checking post presence
-        if (!post) return res.status(400).json({ success: false, message: "Post not found" })
+        if (!post) return res.status(404).json({ success: false, message: "Post not found" })
 
-        if (post.owner.toString !== req.user._id.toString()) return res.status(401).json({ success: false, message: "Unauthoraized" })
+        // Check the post owner and deletion user 
+        if (post.owner.toString() !== req.user._id.toString()) return res.status(401).json({ success: false, message: "Unauthoraized" })
+        await post.deleteOne()
 
-        await post.remove()
-        res.status(200).json({success:true,message:"Post Deleted"})
+        // Find user 
+        const user = await User.findById(req.user._id)
+        // Find index of the post
+        const index = user.posts.indexOf(req.params.id)
+        // Delete the Post
+        user.posts.splice(index, 1)
+        await user.save()
+
+        res.status(200).json({ success: true, message: "Post Deleted" })
 
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message })
@@ -56,8 +66,7 @@ exports.deletePost = async (req, res) => {
 }
 
 
-
-// Like and Unlike Post
+// Like and Unlike Post [get request]
 exports.likeAndUnlikePost = async (req, res) => {
     try {
         // Finding Post
--- Backend/routes/Post.js
+++ Backend/routes/Post.js
@@ -1,10 +1,15 @@
 // API routes creation and using controller func's 
 const express = require("express");
-const { createPost, likeAndUnlikePost } = require("../controller/Post");
+const { createPost, likeAndUnlikePost, deletePost } = require("../controller/Post");
 const { isAuthenticate } = require("../middlewares/auth");
 const router = express.Router()
 
-router.route("/post/upload").post(isAuthenticate, createPost);
-router.route("/post/:id").get(isAuthenticate, likeAndUnlikePost)
+router
+    .route("/post/upload")
+    .post(isAuthenticate, createPost);
+router
+    .route("/post/:id")
+    .get(isAuthenticate, likeAndUnlikePost)
+    .delete(isAuthenticate, deletePost)
 
 module.exports = router
