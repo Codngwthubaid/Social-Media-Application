ed263b0dbf3030548d4c40ff6f3db81246d04146
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-15 15:31:40 +0530

    working on deletePost Func

--- Backend/controller/Post.js
+++ Backend/controller/Post.js
@@ -1,6 +1,7 @@
 const Post = require("../models/Post")
 const User = require("../models/User")
 
+// Post Creation
 exports.createPost = async (req, res) => {
     try {
         // Post details
@@ -36,10 +37,29 @@ exports.createPost = async (req, res) => {
 }
 
 
+// Post Deletion
+exports.deletePost = async (req, res) => {
+    try {
+        // finding post 
+        const post = await Post.findById(req.params.id)
+        // checking post presence
+        if (!post) return res.status(400).json({ success: false, message: "Post not found" })
 
+        if (post.owner.toString !== req.user._id.toString()) return res.status(401).json({ success: false, message: "Unauthoraized" })
+
+        await post.remove()
+        res.status(200).json({success:true,message:"Post Deleted"})
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
+
+
+
+// Like and Unlike Post
 exports.likeAndUnlikePost = async (req, res) => {
     try {
-
         // Finding Post
         const post = await Post.findById(req.params.id)
 
