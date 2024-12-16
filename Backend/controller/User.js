4fb96f53fca252792089803d20579cac535388b6
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-17 00:29:54 +0530

    Get Profile Functionality Completes

--- Backend/controller/User.js
+++ Backend/controller/User.js
@@ -247,7 +247,10 @@ exports.deleteProfile = async (req, res) => {
 exports.getProfile = async (req, res) => {
     try {
         const user = await User.findById(req.user._id).populate("posts")
+        res.status(200).json({ success: true, user })
     } catch (error) {
         res.status(500).json({ success: false, message: error.message })
     }
 }
+
+
