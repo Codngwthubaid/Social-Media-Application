5fc495cc88dde39d4eb9b36aafeaca97d07c2d23
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-16 15:09:30 +0530

    Post Updation Completes

--- Backend/controller/Post.js
+++ Backend/controller/Post.js
@@ -125,7 +125,14 @@ exports.getFollowedUserPost = async (req, res) => {
 // Update Post
 exports.updatePost = async (req, res) => {
     try {
+        const post = await Post.findById(req.params.id)
+        if (!post) return res.status(400).json({ success: false, message: "Post not found" })
 
+        if(post.owner.toString() !== req.user._id.toString()) return res.status(400).json({success:false,message : "Unauthorized"})
+        post.caption = req.body.caption
+        await post.save()
+
+        res.status(200).json({success:true,message:"Caption Successfully Updated"})
     } catch (error) {
         res.status(500).json({ success: false, message: error.message })
     }
--- Backend/routes/Post.js
+++ Backend/routes/Post.js
@@ -1,6 +1,6 @@
 // API routes creation and using controller func's 
 const express = require("express");
-const { createPost, likeAndUnlikePost, deletePost, getFollowedUserPost } = require("../controller/Post");
+const { createPost, likeAndUnlikePost, deletePost, getFollowedUserPost, updatePost } = require("../controller/Post");
 const { isAuthenticate } = require("../middlewares/auth");
 const router = express.Router()
 
@@ -9,11 +9,12 @@ router
     .post(isAuthenticate, createPost);
 router
     .route("/post/:id")
+    .put(isAuthenticate, updatePost)
     .get(isAuthenticate, likeAndUnlikePost)
     .delete(isAuthenticate, deletePost)
-
 router
     .route("/post")
     .get(isAuthenticate, getFollowedUserPost)
+
 
 module.exports = router
