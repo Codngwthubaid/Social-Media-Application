3d9269b35790003aaf0a6cef84a0a03fad820f32
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-18 13:24:37 +0530

    Start working on frontend : routering setup completes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <App />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: '/about',
+    element: (
+      <>
+        <App />
+        <About />
+      </>
+    ),
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
