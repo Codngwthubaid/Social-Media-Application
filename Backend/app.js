25463d810b0f759fc371bacfa1c49581bdf2ad11
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-22 23:24:27 +0530

    Binding the frontend to backend , but faces difficulitie

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 const dotenv = require("dotenv")
 const app = express()
 
 if (process.env.NODE_ENV !== "production") dotenv.config({ path: "Backend/config/.env" })
 
-
 // Using Middleware
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,8 @@ const { connectDB } = require("./config/database");
 
 connectDB()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running at http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 
 })
