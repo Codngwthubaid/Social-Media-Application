eaf4d7ad2b2faa0279bbe87d751fab87de5eb257
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-22 23:27:24 +0530

    Working on frontend for making the proxy to connect with backend

--- Frontend/src/Actions/User.js
+++ Frontend/src/Actions/User.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export const login = (email, password) => async (dispatch) => {
-
     try {
         dispatch({ type: 'loginRequest' })
         const { data } = await axios.post("/api/v1/login", { email, password }, { headers: { 'Content-Type': 'application/json' } })
