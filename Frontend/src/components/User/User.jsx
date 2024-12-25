e683ab129863d182c361cc08b409fc10d17d738d
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-25 19:27:25 +0530

    Working on Account Page

--- Frontend/src/components/User/User.jsx
+++ Frontend/src/components/User/User.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const User = ({ userId, name, avator }) => {
+const User = ({ userId, name, avatar }) => {
     return (
         <div>
             <Link to={`/user/${userId}`}>
                 <div className='flex items-center p-4 border-b'>
                     <img
                         className='h-12 w-12 rounded-full object-cover'
-                        src={avator}
+                        src={avatar}
                         alt={name}
                     />
                     <p className='ml-4'>{name}</p>
