960afa7309c2868f4e359227bf793eabc855fde7
Codngwthubaid <codngwthubaid@gmail.com>
2025-01-27 12:45:47 +0530

    Delete the folder due to unnconfigration

--- Backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-exports.connectDB = () => {
-    mongoose
-        .connect(process.env.MONGO_URL)
-        .then((con) => console.log(`DB connection setup : ${con.connection.host}`))
-        .catch((err) => console.log(err))
-}
